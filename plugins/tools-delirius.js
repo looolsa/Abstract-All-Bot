d62d7c14123299c2c0ae67ea187c329b2ce04eee
Mr. Shady <143818081+looolsa@users.noreply.github.com>
2024-09-01 16:01:01 -0600

    Add/Update plugins/tools-delirius.js

---
+++ plugins/tools-delirius.js
@@ -0,0 +1,32 @@
+import fetch from 'node-fetch';
+import axios from 'axios';
+import translate from '@vitalets/google-translate-api';
+import {Configuration, OpenAIApi} from 'openai';
+const configuration = new Configuration({organization: global.openai_org_id, apiKey: global.openai_key});
+const openaiii = new OpenAIApi(configuration);
+const handler = async (m, {conn, text, usedPrefix, command}) => {
+if (usedPrefix == 'a' || usedPrefix == 'A') return;
+if (!text) throw `*[ 💠 ] Ingresa una petición para acceder a la ia "Delirius"*`    
+
+if (command == 'delirius') {
+try {
+conn.sendPresenceUpdate('composing', m.chat);
+let gpt = await fetch(`https://deliriusapi-official.vercel.app/ia/gptweb?text=${text}`) 
+let res = await gpt.json()
+await m.reply(res.gpt)
+} catch {
+try {
+let gpt = await fetch(`https://deliriusapi-official.vercel.app/ia/chatgpt?q=${text}`)
+let res = await gpt.json()
+await m.reply(res.data)
+} catch {
+}}}
+
+if (command == 'delirius2') {
+conn.sendPresenceUpdate('composing', m.chat);
+let gpt = await fetch(`https://deliriusapi-official.vercel.app/ia/gptweb?text=${text}`) 
+let res = await gpt.json()
+await m.reply(res.gpt)
+}}
+handler.command = /^(delirius|delirius2)$/i;
+export default handler;
