d4d06c06f9bcea2f9d2246333d114d16edf741a5
Mr. Shady <143818081+looolsa@users.noreply.github.com>
2024-09-01 16:02:15 -0600

    Add/Update plugins/main-horny.js

---
+++ plugins/main-horny.js
@@ -0,0 +1,82 @@
+import fetch from 'node-fetch';
+const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
+if (!db.data.chats[m.chat].modohorny && m.isGroup) throw `*❍💠 𝙿𝚊𝚛𝚊 𝚞𝚝𝚒𝚕𝚒𝚣𝚊𝚛 𝚎𝚜𝚝𝚎 𝚌𝚘𝚖𝚊𝚗𝚍𝚘, 𝚎𝚜 𝚗𝚎𝚌𝚎𝚜𝚊𝚛𝚒𝚘 𝚑𝚊𝚋𝚒𝚕𝚒𝚝𝚊𝚛 𝚕𝚘𝚜 𝚌𝚘𝚖𝚊𝚗𝚍𝚘𝚜 _+18_. 𝙿𝚊𝚛𝚊 𝚊𝚌𝚝𝚒𝚟𝚊𝚛𝚕𝚘𝚜 𝚎𝚗 𝚎𝚜𝚝𝚎 𝚌𝚑𝚊𝚝, 𝚎𝚜𝚌𝚛𝚒𝚋𝚊 _${usedPrefix}enable modohorny_*`; 
+  try {
+    const pp = imagen5;
+    const d = new Date(new Date + 3600000);
+    const locale = 'es';
+    const week = d.toLocaleDateString(locale, {weekday: 'long'});
+    const date = d.toLocaleDateString(locale, {day: 'numeric', month: 'long', year: 'numeric'});
+    const _uptime = process.uptime() * 1000;
+    const uptime = clockString(_uptime);
+    const user = global.db.data.users[m.sender];
+    const {money, joincount} = global.db.data.users[m.sender];
+    const {exp, limit, level, role} = global.db.data.users[m.sender];
+    const rtotalreg = Object.values(global.db.data.users).filter((user) => user.registered == true).length;
+    const more = String.fromCharCode(8206);
+    const readMore = more.repeat(850);
+    const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
+    const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
+    const document = doc[Math.floor(Math.random() * doc.length)];
+    const str = `┏┅┅┓ł  ☬ㅤㅤㅤ✦∮✦ㅤㅤㅤ☬ ł ┏┅┅┓
+┣┱  🝃ɞ *𝙰𝚋𝚜𝚝𝚛𝚊𝚌𝚝- 𝙰𝚕𝚕 ł 𝙱𝚘𝚝* ʚ🝃  ┲┫
+┗┅┅┚ł  ☬ㅤㅤㅤ✦∮✦ㅤㅤㅤ☬ ł ┗┅┅┛
+
+┍⊰᯽⊱┈──╌❊ㅤ⇑ㅤ❊╌──┈⊰᯽⊱
+│『 *+ 1 8 / 𝙷 𝙾 𝚃* 』
+┝⊰᯽⊱┈┈┈┈─────┴┈─┐
+├⊰ 🝉.- _${usedPrefix}porno_
+├⊰ 🝉.- _${usedPrefix}pack_
+├⊰ 🝉.- _${usedPrefix}pack2_
+├⊰ 🝉.- _${usedPrefix}pack3_
+├⊰ 🝉.- _${usedPrefix}videoxxx_
+├⊰ 🝉.- _${usedPrefix}videolesbixxx_
+├⊰ 🝉.- _${usedPrefix}tetas_
+├⊰ 🝉.- _${usedPrefix}booty_
+├⊰ 🝉.- _${usedPrefix}imagenlesbians_
+├⊰ 🝉.- _${usedPrefix}panties_
+├⊰ 🝉.- _${usedPrefix}pechos_
+├⊰ 🝉.- _${usedPrefix}pene_
+├⊰ 🝉.- _${usedPrefix}randomxxx_
+├⊰ 🝉.- _${usedPrefix}hentai_
+├⊰ 🝉.- _${usedPrefix}yaoi_
+├⊰ 🝉.- _${usedPrefix}yaoi2_
+├⊰ 🝉.- _${usedPrefix}yuri_
+├⊰ 🝉.- _${usedPrefix}yuri2_
+├⊰ 🝉.- _${usedPrefix}trapito_
+├⊰ 🝉.- _${usedPrefix}nsfwloli_
+├⊰ 🝉.- _${usedPrefix}nsfworgy_
+├⊰ 🝉.- _${usedPrefix}nsfwfoot_
+├⊰ 🝉.- _${usedPrefix}nsfwass_
+├⊰ 🝉.- _${usedPrefix}nsfwbdsm_
+├⊰ 🝉.- _${usedPrefix}nsfwcum_
+├⊰ 🝉.- _${usedPrefix}nsfwero_
+├⊰ 🝉.- _${usedPrefix}nsfwfemdom_
+├⊰ 🝉.- _${usedPrefix}nsfwglass_
+├⊰ 🝉.- _${usedPrefix}hentaipdf *«texto»*_
+├⊰ 🝉.- _${usedPrefix}hentaisearch *«texto»*_
+├⊰ 🝉.- _${usedPrefix}ecchi_
+├⊰ 🝉.- _${usedPrefix}rule34_
+┕⊰᯽⊱┈──╌❊ㅤ⇓ㅤ❊╌──┈⊰᯽⊱
+*└*B̷y̷. 𝙰𝚋𝚜𝚝𝚛𝚊𝚌𝚝 - 𝙰𝚕𝚕 - 𝙱𝚘𝚝`.trim();
+    if (m.isGroup) {
+      await conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: m});
+    } else {
+      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
+      await conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
+    }
+  } catch {
+    conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙼𝙴𝙽𝚄 𝚃𝙸𝙴𝙽𝙴 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁 𝚈 𝙽𝙾 𝙵𝚄𝙴 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝙴𝙽𝚅𝙸𝙰𝚁𝙻𝙾, 𝚁𝙴𝙿𝙾𝚁𝚃𝙴𝙻𝙾 𝙰𝙻 𝙿𝚁𝙾𝙿𝙸𝙴𝚃𝙰𝚁𝙸𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃*', m);
+  }
+};
+handler.command = /^(menu18|pornito|menuh)$/i;
+handler.exp = 50;
+handler.fail = null;
+export default handler;
+
+function clockString(ms) {
+  const h = isNaN(ms) ? '--' : Math.floor(ms / 3600000);
+  const m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60;
+  const s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60;
+  return [h, m, s].map((v) => v.toString().padStart(2, 0)).join(':');
+}
