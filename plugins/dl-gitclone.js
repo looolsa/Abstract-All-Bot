ffcb3f328914f0b2af69fb5ccb29bfe4516286f1
Mr. Shady <143818081+looolsa@users.noreply.github.com>
2024-09-01 16:03:30 -0600

    Add/Update plugins/dl-gitclone.js

---
+++ plugins/dl-gitclone.js
@@ -0,0 +1,16 @@
+import fetch from 'node-fetch';
+const regex = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i;
+const handler = async (m, {args, usedPrefix, command}) => {
+  if (!args[0]) throw `*[ 💠 ] Ingresa un link de GitHub.*\n_⚕️.- Ejemplo:_ *${usedPrefix + command}* https://github.com/looolsa/Abstract-All-Bot`;
+  if (!regex.test(args[0])) throw '*[ 💠 ] Asegurate de ingresar el link correctamente.*';
+  let [_, user, repo] = args[0].match(regex) || [];
+  repo = repo.replace(/.git$/, '');
+  const url = `https://api.github.com/repos/${user}/${repo}/zipball`;
+  const filename = (await fetch(url, {method: 'HEAD'})).headers.get('content-disposition').match(/attachment; filename=(.*)/)[1];
+  m.reply(`*[ 💠 ] Espera un momento.* _(Si el archivo no es enviado, puede ser porque es demasiado pesado, o no esté disponible)_`);
+  conn.sendFile(m.chat, url, filename, null, m);
+};
+handler.help = ['gitclone <url>'];
+handler.tags = ['downloader'];
+handler.command = /gitclone/i;
+export default handler;
