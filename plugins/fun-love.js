1c16b707a69296b992fea333acf07c925253592a
Mr. Shady <143818081+looolsa@users.noreply.github.com>
2024-09-01 16:03:12 -0600

    Add/Update plugins/fun-love.js

---
+++ plugins/fun-love.js
@@ -0,0 +1,11 @@
+const handler = async (m, {conn, command, text}) => {
+  const love = `*❤️❤️ MEDIDOR DE AMOR ❤️❤️*
+*El amor de ${text} por ti es de* *${Math.floor(Math.random() * 100)}%* *de un 100%*
+*Deberias pedirle que sea tu  novia/o ?*
+`.trim();
+  m.reply(love, null, {mentions: conn.parseMention(love)});
+};
+handler.help = ['love'];
+handler.tags = ['fun'];
+handler.command = /^(love)$/i;
+export default handler;
