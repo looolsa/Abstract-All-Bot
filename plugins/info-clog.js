d721b424f70d92398e7163a3089f5ad49d2643c2
Mr. Shady <143818081+looolsa@users.noreply.github.com>
2024-09-01 16:02:24 -0600

    Add/Update plugins/info-clog.js

---
+++ plugins/info-clog.js
@@ -0,0 +1,80 @@
+let handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
+let txt = `┍⊰᯽⊱┈──╌❊ㅤ⇑ㅤ❊╌──┈⊰᯽⊱
+│『 𝙲 𝙷 𝙰 𝙽 𝙶 𝙴 𝙻 𝙾 𝙶 』
+┕⊰᯽⊱┈┈┈┈─────┴┈᯽⊱
+
+𝑴𝒖𝒚 𝑩𝒖𝒆𝒏𝒂𝒔, 𝒖𝒔𝒖𝒂𝒓𝒊𝒐.
+𝑬𝒔𝒕𝒆 𝒆𝒔 𝒖𝒏 𝒆𝒔𝒑𝒂𝒄𝒊𝒐 𝒅𝒆𝒅𝒊𝒄𝒂𝒅𝒐 𝒂 𝒊𝒏𝒇𝒐𝒓𝒎𝒂𝒓 𝒂𝒄𝒆𝒓𝒄𝒂 𝒅𝒆 𝒕𝒐𝒅𝒐𝒔 𝒍𝒐𝒔 𝒄𝒂𝒎𝒃𝒊𝒐𝒔 𝒒𝒖𝒆 𝒔𝒆 𝒉𝒂𝒏 𝒓𝒆𝒂𝒍𝒊𝒛𝒂𝒅𝒐 𝒂 𝒍𝒐 𝒍𝒂𝒓𝒈𝒐 𝒅𝒆 𝒍𝒂𝒔 𝑩𝒆𝒕𝒂𝒔 𝒚 𝑷𝒓𝒆𝒗𝒊𝒆𝒘'𝒔 𝒅𝒆𝒍 𝒃𝒐𝒕. 𝑬𝒔 𝒊𝒎𝒑𝒐𝒓𝒕𝒂𝒏𝒕𝒆 𝒎𝒆𝒏𝒄𝒊𝒐𝒏𝒂𝒓 𝒒𝒖𝒆 𝒂𝒍𝒈𝒖𝒏𝒂𝒔 𝒄𝒐𝒔𝒂𝒔 𝒇𝒖𝒆𝒓𝒐𝒏 𝒂𝒈𝒓𝒆𝒈𝒂𝒅𝒂𝒔, 𝒎𝒐𝒅𝒊𝒇𝒊𝒄𝒂𝒅𝒂𝒔, 𝒚/𝒐 𝒆𝒍𝒊𝒎𝒊𝒏𝒂𝒅𝒂𝒔.
+𝑨 𝒄𝒐𝒏𝒕𝒊𝒏𝒖𝒂𝒄𝒊𝒐́𝒏, 𝒖𝒏𝒂 𝒍𝒊𝒔𝒕𝒂 𝒅𝒆 𝒕𝒐𝒅𝒐 𝒂𝒏𝒕𝒆𝒔 𝒎𝒆𝒏𝒄𝒊𝒐𝒏𝒂𝒅𝒐:
+
+💠⚕ 𝙸𝚖𝚙𝚕𝚎𝚖𝚎𝚗𝚝𝚊𝚌𝚒𝚘𝚗𝚎𝚜:
+
+💠.- La gran mayoría de apartados en el bot ahora cuentan con una interfaz actualizada acorde a las características de este.
+
+💠.- Se han implementado 6 nuevas inteligencias artificiales:
+
+┏━━━━━┓
+┣ *⚕️.- ${usedPrefix}delirius*
+┣ *⚕️.- ${usedPrefix}gemini*
+┣ *⚕️.- ${usedPrefix}chatgpt*
+┣ *⚕️.- ${usedPrefix}openai*
+┣ *⚕️.- ${usedPrefix}copilot*
+┣ *⚕️.- ${usedPrefix}gpt4*
+┗━━━━━━━━━━━━━━━━┛
+
+Su principal propósito es llevarlas a la par con la nueva inteligencia artificial implementada a WhatsApp, Meta AI.
+
+💠.- Se ha implementado una nueva inteligencia artificial, _"${usedPrefix}chatgpt /skv2"_.
+Su nombre es Xexploit, tiene la capacidad de responder cualquier petición de los usuarios, y cuando digo "cualquiera", es literalmente cualquiera. Solo que, tiende a insultar a absolutamente todos los miembros (activos) de Abstract-All M.D.
+
+💠.- Ahora existen 3 tipos de play, estos son:
+
+┏━━━━━┓
+┣ *⚕️.- ${usedPrefix}play*
+┣ *⚕️.- ${usedPrefix}pl*
+┣ *⚕️.- ${usedPrefix}pld*
+┗━━━━━━━━━━━━━━━━┛
+
+Cada play cuenta con su respectiva función para descarga de videos. El propósito de esta implementación, es tratar equilibrar el uso de este comando, y por si algún día se rompe alguno, tengamos otro de repuesto.
+
+💠.- Los comandos para efectos para los audios vuelven a funcionar con normalidad.
+
+💠.- Después de un tiempo relativamente indefinido, el comando _"${usedPrefix}tovideo"_ vuelve a funcionar con normalidad.
+
+💠.- Se ha agregado los siguientes comandos:
+
+┏━━━━━┓
+┣ *⚕️.- ${usedPrefix}jalamela*
+┣ *⚕️.- ${usedPrefix}follar*
+┣ *⚕️.- ${usedPrefix}aplauso*
+┗━━━━━━━━━━━━━━━━┛
+
+¿Su utilidad? No me pregunten a mí, solo usen los comandos y ahí me avisan cómo les va.
+
+💠.- Se ha solucionado un error el cuál ocasionaba que al usar el comando _"${usedPrefix}chamba"_ se reseteara el tiempo del comando _"${usedPrefix}work"_.
+
+💠.- Los comandos _"${usedPrefix}work"_, y _"${usedPrefix}chamba"_ ahora entregan la cantidad de experiencia correspondiente.
+
+*⚕.- ACTUALIZACIÓN IMPORTANTE*
+
+💠.- El menú ha redistribuido los apartados de este, haciendo que puedan consultarse con su respectivo comando. Todos estos serán mostrados en el menú principal. 
+
+🔱🔹 𝙰𝚋𝚘𝚕𝚒𝚌𝚒𝚘𝚗𝚎𝚜:
+
+🔱.- Se ha solucionado un error visual, el cuál aparecía en la imagen de despedida a un integrante al salir de un chat grupal.
+
+🔱.- Más funciones de los menús, las cuales eran carentes de utilidad continua, se han eliminado.
+
+Agradecimientos especiales:
+1. I'm Fz ~.
+Contacto: https://wa.me/5212431268546?text=Tesis,+mamame+la+riatota+jajdkakdksls
+
+💠⚕ Te invito a unirte al grupo oficial, donde te enterarás de todas las novedades con respecto al bot: https://chat.whatsapp.com/CN3YieShy2S3LnVRJ7aPPM
+
+*└*B̷y̷. 𝙰𝚋𝚜𝚝𝚛𝚊𝚌𝚝𝚊𝚕𝚕 - 𝙱𝚘𝚝`.trim();
+
+await conn.sendMessage(m.chat, { react: { text: '🗣', key: m.key }})
+          await conn.sendFile(m.chat, imagen1, 'clog.jpg', txt.trim(), m)
+}
+handler.command = ['clog', 'changelog']
+export default handler
