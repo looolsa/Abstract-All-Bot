958f89aadc234af076e2a1c019b92de7176a4f45
Mr. Shady <143818081+looolsa@users.noreply.github.com>
2024-09-01 16:03:28 -0600

    Add/Update plugins/dl-pl2.js

---
+++ plugins/dl-pl2.js
@@ -0,0 +1,14 @@
+let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, command }) => {
+  if (!args || !args[0]) throw `*[ 💠 ] Complementa tu peticion con algún enlace de YouTube.*\n_(Puedes hacer una búsqueda utilizando el comando ${usedPrefix}yts)_
+ ⚕️.- Ejemplo: .pl2 https://youtu.be/sBKR6aUorzA?si=TmC01EGbXUx2DUca`;
+  if (!args[0].match(/youtu/gi)) throw `*[ 💠 ] Asegúrate de ingresar un enlace de YouTube.*`;
+  
+ let chat = global.db.data.chats[m.chat]
+ await conn.sendMessage(m.chat, { react: { text: '⚕️', key: m.key }})
+ let res = await ytmp44(args[0]);
+conn.sendFile(m.chat, res.resultados.descargar, 'video.mp4', '', m)
+await conn.sendMessage(m.chat, { react: { text: '💠', key: m.key }})
+};
+handler.tags = ['dl']
+handler.command = ['plv', 'pl2', 'ytv']
+export default handler
