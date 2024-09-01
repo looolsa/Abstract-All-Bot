2aafaff33db2e2c512eb53514b06ea0af9fe3b5f
Mr. Shady <143818081+looolsa@users.noreply.github.com>
2024-09-01 16:01:02 -0600

    Add/Update plugins/tools-GPT-4.js

---
+++ plugins/tools-GPT-4.js
@@ -0,0 +1,14 @@
+let handler = async (m, { text, usedPrefix, command }) => {
+  if (!text) throw `*[ 💠 ] Ingresa una petición para acceder al GPT-4.*`;
+
+const syms1 = `Actuaras como un Bot de WhatsApp.`
+let a = await fetch(`https://api.cafirexos.com/api/chatgpt?text=${text}&name=${m.name}&prompt=${syms1}`)
+let res = await a.json()
+  
+  await conn.sendMessage(m.chat, {
+    text: res.resultado }, { quoted: m })
+  
+};
+handler.command = handler.help = ['gpt4'];
+handler.tags = ['tools'];
+export default handler
