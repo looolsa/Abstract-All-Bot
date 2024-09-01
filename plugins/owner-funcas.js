4b579aa31cffcd62a9967a018c2dead19a5a8d4e
Mr. Shady <143818081+looolsa@users.noreply.github.com>
2024-09-01 16:01:44 -0600

    Add/Update plugins/owner-funcas.js

---
+++ plugins/owner-funcas.js
@@ -0,0 +1,7 @@
+const handler = async (m, {conn, command}) => {
+  console.log('XDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD');
+  m.reply('No funca bb');
+};
+handler.command = /^(handerror)$/i;
+handler.owner = true;
+export default handler;
