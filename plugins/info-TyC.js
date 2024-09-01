e655392a6da6d50fb84796e63e8e73e640a0a8d9
Mr. Shady <143818081+looolsa@users.noreply.github.com>
2024-09-01 16:02:26 -0600

    Add/Update plugins/info-TyC.js

---
+++ plugins/info-TyC.js
@@ -0,0 +1,42 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.terminos);
+};
+handler.command = ['tyc']
+export default handler;
+
+global.terminos = `
+_Se recomienda tener la suficiente comprensión lectora para entender las siguientes frases en este escríto._
+_Yo, ඞ 𝐿.𝐴𝑛 ༆, trataré de ser lo más directo posible para lograr la comprensión de lo que se mostrará a continuación._
+
+*[ ⚠️ ] Términos de privacidad*
+
+_1. El tipo de información que sea intercambiada con el bot, no será compartido a ningún tercero._
+_2. Los elementos actuales, como lo pueden ser las imágenes, videos, stickers, audios, etc. No serán compartidos con ningún tercero._
+_3. Los números telefónicos que tenga cualquier tipo de contacto con el bot, no serán compartidos con ningún tercero._
+_4. Datos relativamente importantes como lo pueden ser; tarjetas, ubicaciones, direcciones y demás, son eliminados cada 1:00 min de la base, y no se compartirán con ningún tercero._
+_5. Absolutamente todas las conversaciones son eliminadas en un determinado tiempo. Nunca se realizará un backup (copia de seguridad) de ningún tipo de conversación, o peticiones por parte de los usuarios._
+
+*[ ⚠️ ] Términos de uso*
+
+_1. Yo, 𝐿.𝐴𝑛 ༆, administrando el bot por parte del fallecido Mr. Shady, me deslindo sobre cualquier responsabilidad acerca de un mal uso que se le pueda dar a este, ya sean peticiones, interacciones, conversaciones, etc._
+_2. Yo no cargo con la responsabilidad de la ignorancia y/o desconocimiento del "tipo de uso" que se le pueda dar a este._
+_3. El Bot no puede permanecer activo las 24h debido a problemasa con el host actual, si se apaga por alguna razón, favor de avisarme directamente para evitar cualquier tipo de confusión._
+_4. Al igual que los puntos anteriores, no me responsabilizo sobre los números que puedan caer en soporte técnico por un uso indebido del este._
+_5. Todo el material que se usa para las interacciones, ya sean audios, fotos, stickers, música y demás, son de uso público. La base original de este, le pertenece al señor Bruno Sobrino, la cual ha sido modificada para no lucrar sus elementos._
+
+*[ 💠 ] ¿Algún mensaje por parte del Bot?*
+_-• Si en algún momento recibes un mensaje a través de este número, y no es una respuesta a un comando, posiblemente sea nuestra amiga Sol. El número actualmente es manejado por ella, y se te pide que no le faltes al respeto._
+
+*[ ⚕️ ] Agradecimientos especiales:*
+*-• Shadow Bot.*
+*-• FzTeis.*
+*-• Q. E. P. D: Mr. Shady.*
+*-• Bruno Sobrino y a todo su equipo de producción.*
+
+*[ ⚜️ ] ¿Dudas?*
+_-• Si tienes alguna duda referente a todo lo antes mencionado, trataré de responderla personalmente en mi número privado, el cual te dejo aquí abajo. Cualquier mensaje en tono burlesco será ignorado._
+
+• wa.me/+525534215245
+*• 𝐿.𝐴𝑛 ༆*
+
+*💠⚕️𝙰𝚋𝚜𝚝𝚛𝚊𝚌𝚝-𝙰𝚕𝚕 - 𝙱𝚘𝚝.*`;
