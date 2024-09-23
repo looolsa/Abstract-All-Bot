098d38cd0202e0484ff11c4efbd3b2043dfbd90f
Mr. Shady <143818081+looolsa@users.noreply.github.com>
2024-09-22 18:19:24 -0600

    Add/Update plugins/randomi-dios.js

--- plugins/randomi-dios.js
+++ plugins/randomi-dios.js
@@ -1,9 +1,10 @@
 export async function before(m, { conn, isAdmin, isBotAdmin }) {
+    let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
     if (m.isBaileys && m.fromMe) return !0;
     if (!m.isGroup) return !1;
 
     const gatoRegex = /dios/i;
-
+    
     if (gatoRegex.test(m.text)) {
         await conn.sendFile(m.chat, 'https://telegra.ph/file/2befce16c18d72a4d11a3.jpg', 'Dios.jpg', 'xdxdxdxdxdxdxdxdxdxd', m);
     }
