256eec1e7f4c5e2d37bb239ee1da32c4654956aa
Mr. Shady <143818081+looolsa@users.noreply.github.com>
2024-09-22 18:20:26 -0600

    Add/Update plugins/gp-totag.js

--- plugins/gp-totag.js
+++ plugins/gp-totag.js
@@ -2,7 +2,7 @@
 let handler = async (m, { conn, text, participants}) => {
 	
     let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-    if (!m.quoted) throw `✳️ ${mssg.reply}`
+    if (!m.quoted) throw `*[ 💠 ] Responde al mensaje o complementa la mención.*`
     conn.sendMessage(m.chat, { forward: m.quoted.fakeObj, mentions: users } )
 }
 
