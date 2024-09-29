5ea87bd1dc7298c0f2fbf31c12999232bb85f274
Mr. Shady <143818081+looolsa@users.noreply.github.com>
2024-09-29 16:29:15 -0600

    Delete plugins/funcas.js

--- plugins/funcas.js
+++
@@ -1,6 +0,0 @@
-const handler = async (m, {conn, command}) => {
-  console.log('XDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD');
-  m.reply('No funca bb');
-};
-handler.command = ['funcas']
-export default handler;
